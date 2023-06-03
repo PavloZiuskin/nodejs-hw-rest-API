306874b2c70497fe4b82f5cd4c86804fed8cbfb6
PavloZiuskin <passzuskin@gmail.com>
2023-06-03 16:52:59 +0300

    add-multer-and-gravatar

--- routes/api/authRoute.js
+++ routes/api/authRoute.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const upload = require("../../midleware/upload");
+
 const {
   register,
   login,
   logout,
   getCurrent,
+  updateAvatar,
 } = require("../../controllers/authControllers");
 
 const authenticate = require("../../midleware/authenticate");
@@ -14,15 +17,19 @@ const validateBody = require("../../midleware/validationSchema");
 
 const { schemas } = require("../../models/User");
 
-const {handleUserByIdError} = require("../../helpers")
+const { handleUserByIdError } = require("../../helpers");
 
-
-router.post("/register", validateBody(schemas.userRegisrtationSchema), register);
+router.post(
+  "/register",
+  validateBody(schemas.userRegisrtationSchema),
+  register
+);
 
 router.post("/login", validateBody(schemas.userLoginSchema), login);
 
 router.get("/current", authenticate, handleUserByIdError, getCurrent);
 
 router.post("/logout", authenticate, handleUserByIdError, logout);
+router.patch("/avatars", authenticate, upload.single("avatar"), updateAvatar);
 
-module.exports = {authRoute: router};
+module.exports = { authRoute: router };
