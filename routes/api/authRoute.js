5baf961843bc2edcdc9625223cd17dcdbb1b1446
PavloZiuskin <passzuskin@gmail.com>
2023-06-10 15:08:21 +0300

    add-sendMail

--- routes/api/authRoute.js
+++ routes/api/authRoute.js
@@ -1,22 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
 const upload = require("../../midleware/upload");
-
 const {
   register,
   login,
   logout,
   getCurrent,
   updateAvatar,
+  verifyEmail,
+  resendVerifyEmail,
 } = require("../../controllers/authControllers");
-
 const authenticate = require("../../midleware/authenticate");
-
 const validateBody = require("../../midleware/validationSchema");
-
 const { schemas } = require("../../models/User");
-
 const { handleUserByIdError } = require("../../helpers");
 
 router.post(
@@ -24,11 +20,14 @@ router.post(
   validateBody(schemas.userRegisrtationSchema),
   register
 );
-
 router.post("/login", validateBody(schemas.userLoginSchema), login);
-
 router.get("/current", authenticate, handleUserByIdError, getCurrent);
-
+router.get("/verify/:verificationToken", verifyEmail);
+router.post(
+  "/verify",
+  validateBody(schemas.joiVerifyEmailSchema),
+  resendVerifyEmail
+);
 router.post("/logout", authenticate, handleUserByIdError, logout);
 router.patch("/avatars", authenticate, upload.single("avatar"), updateAvatar);
 
